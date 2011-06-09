fa27e7bd0682257de0149b0333475ec0c3bcb184
Ryan Brunner <ryan@ryanbrunner.com>
2011-06-09 19:20:54 -0400

    Initial version.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Qrey::Application.config.secret_token = '218706d497007f7f4d3bbde5e1df7a40519102c28e84e8dca3a76d95893653567b23731cef6e7ea346c104ecf294c6d5644a556b4d0fd5f8fc0bdf63b119f8f4'
