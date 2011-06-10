e74e39ee6d64d2390d2b77130345d0cf38267fa9
Ryan Brunner <ryan@ryanbrunner.com>
2011-06-09 20:41:17 -0400

    Added type of QR code to grab.

--- app/controllers/qr_codes_controller.rb
+++ app/controllers/qr_codes_controller.rb
@@ -1,4 +1,9 @@
 class QrCodesController < InheritedResources::Base
   actions :index, :new, :create, :show
   respond_to :html, :json, :xml
+
+  def create
+    @qr_code = QrCode.initialize_for_type(params[:type], params[:qr_code])
+    create!
+  end
 end
