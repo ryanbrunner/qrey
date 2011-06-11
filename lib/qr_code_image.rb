1a289884e84c8bb6d577da5509159997547ad61a
Mike Stok <mike@stok.ca>
2011-06-11 06:51:28 -0400

    Cleans up qr_code_image
    * adds documentation
    * removes unused constants

--- lib/qr_code_image.rb
+++ lib/qr_code_image.rb
@@ -1,19 +1,25 @@
 # Simple class to construct QR images and return them as scaled PNG
 # image data.
-
-require 'rqrcode'
-require 'rmagick'
-
+# 
+# Useful information can be found at:
+# * http://en.wikipedia.org/wiki/QR_Code
+# * http://www.qrcode.com/index-e.html
 class QRCodeImage
+  require 'rqrcode'
+  require 'rmagick'
 
-  LEGAL_SIZES = [1, 2, 3, 4, 10, 40]
-  LEGAL_LEVELS = [:l, :m, :q, :h]
-
-  def initialize(string, size, level)
-    @qr = RQRCode::QRCode.new(string, :size => size, :level => level)
+  # Creates a new QRCodeImage object.
+  #
+  # +string+:: the data to be encoded
+  # +version+:: the version of the QR code (1 .. 40) 
+  # +level+:: ecc level (:l, :m, :q, :h)
+  def initialize(string, version, level)
+    @qr = RQRCode::QRCode.new(string, :size => version, :level => level)
   end
 
-  # returns the QR code as a PNG with each pixel scaled to scale x scale
+  # Returns the QR code as a blob of PNG data.
+  # 
+  # +scale+:: the number of pixels wide & deep to render each QR Code module
   def as_png(scale=5)
     pixels = @qr.modules.size
 
@@ -21,14 +27,14 @@ class QRCodeImage
     img.format = 'PNG'
     img.background_color = 'white'
 
-    @qr.modules.each_with_index do |row, y|
-      row.each_with_index do |dark, x|
-        img.pixel_color(x, y, 'black') if dark
+    pixels.times do |x|
+      pixels.times do |y|
+        if @qr.dark?(x,y)
+          img.pixel_color(x, y, 'black')
+        end
       end
     end
 
-    img.scale!(scale)           
-
-    img.to_blob
+    img.scale(scale).to_blob
   end
 end
